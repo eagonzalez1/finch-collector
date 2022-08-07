660941ad3e6e81cec2953409fae39a76fa6d9cc8
eagonzalez81 <gonzoatc81@gmail.com>
2022-08-07 11:26:20 -0400

    add feeding model

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
+from .forms import FeedingForm
 
 # Define the home view
 def home(request):
@@ -10,4 +12,24 @@ def about(request):
 
 # Add new view
 def finches_index(request):
-  return render(request, 'finches/index.html', { 'finches': finches })
+  finches = Finch.objects.all()
+  feeding_form = FeedingForm()
+  return render(request, 'finches/index.html', { 
+    'finches': finches, 'feeding_form': feeding_form
+    })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
+
+class FinchCreate(CreateView):
+  model = Finch
+  success_url = '/finches/'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches/'
